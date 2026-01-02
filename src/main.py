c768bac0cd0e88463cf72ff6c32e8f825f510dea
matthewbroitman <matthewbroitman@gmail.com>
2026-01-02 17:05:43 -0400

    B: TextNode & TextType  Creation

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextNode, TextType
+
+def main():
+    node = TextNode("All Hail the Dunce Cap",TextType.BOLD,"https://www.youtube.com/watch?v=2znuJkkPj6I")
+    print(node)
+
+main()
