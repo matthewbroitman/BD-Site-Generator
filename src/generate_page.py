450e2f1b51901485537e8e0aa1d2f19e74974858
matthewbroitman <matthewbroitman@gmail.com>
2026-01-25 14:58:57 -0400

    M: Staged to Github Pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -3,7 +3,7 @@ from block_markdown import markdown_to_html_node
 from extract_title import extract_title
 from htmlnode import HTMLNode
 
-def generate_page(from_path,template_path,dest_path):
+def generate_page(from_path,template_path,dest_path,basepath):
     print (f"Reticulating Splines: {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r", encoding="utf-8") as f:
         from_string = f.read()
@@ -14,19 +14,21 @@ def generate_page(from_path,template_path,dest_path):
     title = extract_title(from_string)
     template_string = template_string.replace("{{ Title }}", title)
     template_string = template_string.replace("{{ Content }}",HTMLString)
+    template_string = template_string.replace('href="/','href="' + basepath)
+    template_string = template_string.replace('src="/','src="' + basepath)
     dirpath = os.path.dirname(dest_path)
     if dirpath and not os.path.exists(dirpath):
         os.makedirs(dirpath, exist_ok=True)
     with open(dest_path, "w", encoding="utf-8") as f:
         f.write(template_string)
     
-def generate_page_recursively(dir_path_content,template_path,dest_dir_path):
+def generate_page_recursively(dir_path_content,template_path,dest_dir_path,basepath):
     items = os.listdir(dir_path_content)
     for item in items:
         item_path = os.path.join(dir_path_content,item)
         dest_item_path = os.path.join(dest_dir_path,item.replace(".md",".html"))
         if os.path.isfile(item_path) and item_path.endswith(".md"):
-            generate_page(item_path,template_path,dest_item_path)
+            generate_page(item_path,template_path,dest_item_path,basepath)
         if os.path.isdir(item_path):
-            generate_page_recursively(item_path,template_path,dest_item_path)
+            generate_page_recursively(item_path,template_path,dest_item_path,basepath)
     
--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
 import os
 import shutil
+import sys
 
 
 from copystatic import copy_files_recursive
 from generate_page import generate_page,generate_page_recursively
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
+default_basepath = "/"
 
 def main():
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -16,6 +22,6 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static,dir_path_public)
 
-    generate_page_recursively("./content","./template.html","./public")
+    generate_page_recursively("./content","./template.html",dir_path_public,basepath)
 
 main()
